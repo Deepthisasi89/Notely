6c3e58f5e30628e05fcf8b7c8f664f24f2dea631
Deepthi <dpthisas@gmail.com>
2025-01-02 15:46:00 +0000

    checking context

--- src/notely_lambda.py
+++ src/notely_lambda.py
@@ -6,6 +6,16 @@ dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table('Notely_DB')
 
 def lambda_handler(event, context):
+
+    context = event.get('context', {})
+    http_method = context.get('http-method', None)
+
+    if http_method is None:
+        return {
+            'statusCode': 500,
+            'body': jsons.dumps('Unable to determine HTTP method')
+        }
+
     newnote = create_note()
     table.put_item(
         Item=jsons.dump(newnote)
